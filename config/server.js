1000ef18b0543aa076b72172bf0589f7a7bcd3db
Jacobws01 <jacobwsinclair1234@outlook.com>
2021-02-12 10:16:56 -0800

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '74e66a3255d19b2b31bed600d45acb44'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '0802201b-e9ff-4085-bf0e-b05dabb40c6c'
+};
