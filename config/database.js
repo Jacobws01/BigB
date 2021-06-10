ce5b3e7753c19fd382bd2e2c12b803e466e773c3
Jacobws01 <jacobwsinclair1234@outlook.com>
2021-06-10 08:01:21 -0700

    big commit

--- config/database.js
+++ config/database.js
@@ -1,4 +1,31 @@
-module.exports = ({ env }) => ({
+const parse = requie('pg-connection-string').parse;
+
+module.exports = ({ env }) => {
+  
+  if(env('NODE_ENV') === 'production') {
+    const config = parse(process.env.DATABASE_URL)
+    return {
+      defaultConnection: 'default',
+      connections: {
+        default: {
+          connector: 'bookshelf',
+          settings: {
+            client: 'postgres',
+            host: config.host,
+            port: config.port,
+            database: config.database,
+            username: config.user,
+            password: config.password
+          },
+          options: {
+            ssl: false
+          }
+        }
+      }
+    }
+  }
+
+  return {
   defaultConnection: 'default',
   connections: {
     default: {
@@ -12,4 +39,4 @@ module.exports = ({ env }) => ({
       },
     },
   },
-});
+}};
